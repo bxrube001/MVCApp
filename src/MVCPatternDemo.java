86d4cdc91c2830b2126b80bc29915c5608e33d10
bxrube001 <rubert.benjamen@gmail.com>
2016-03-19 20:33:52 -0400

    Initial commit

---
+++ src/MVCPatternDemo.java
@@ -0,0 +1,29 @@
+/**
+ * Created by brubert on 3/19/16.
+ */
+public class MVCPatternDemo {
+    public static void main(String[] args){
+
+        //fetch student record based on his roll no from the database
+        Student model = retrieveStudentFromDatabase();
+
+        //Create a view: to write student details on console
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(model,view);
+
+        controller.updateView();
+
+        //update model data
+        controller.setStudentName("Ben");
+
+        controller.updateView();
+    }
+
+    private static Student retrieveStudentFromDatabase(){
+        Student student = new Student();
+        student.setName("Jim");
+        student.setRollNo("10");
+        return student;
+    }
+}
---
+++ src/StudentView.java
@@ -0,0 +1,10 @@
+/**
+ * Created by brubert on 3/19/16.
+ */
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: "+ studentName);
+        System.out.println("Roll No: "+ studentRollNo);
+    }
+}
